7437df236ff0f533c46e1bcb609c328f41ab9e73
KelviNosse <kelvinjosse@gmail.com>
2017-07-29 17:54:06 -0600

    Added vuematerial dependency

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,23 +1,13 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div id="hello">
+  <md-toolbar>
+    <h1 class="md-title">My Title</h1>
+  </md-toolbar>
+  
+  <div class="main-content">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo, rerum? Error sunt, aperiam dolores, atque expedita molestiae tenetur. Quis eveniet accusamus velit explicabo adipisci reiciendis modi eaque quas, officia excepturi.</p>
   </div>
+</div>
 </template>
 
 <script>
@@ -31,7 +21,6 @@ export default {
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
   font-weight: normal;
@@ -49,5 +38,9 @@ li {
 
 a {
   color: #42b983;
+}
+
+.main-content {
+  padding: 16px;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
+import VueMaterial from 'vue-material';
 import Router from 'vue-router';
 import Hello from '@/components/Hello';
+import '../../node_modules/vue-material/dist/vue-material.css';
 
+Vue.use(VueMaterial);
 Vue.use(Router);
 
 export default new Router({
